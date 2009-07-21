28f36a63cf24d4472d0e9f70a3f1155e0dff267d
Hayato Fujii <hayato dot fujii at gmail dot com>
2009-07-21 16:05:02 +0000

    Alterações mínimas no B.

--- projetoB/tetris.cpp
+++ projetoB/tetris.cpp
@@ -6,7 +6,7 @@ roadmap
 -> criar os blocos
 -> fazer eles cairem com o tempo controlado
 -> arranjar um jeito de controle
--> detectar quanto que uma linha da board está cheia
+-> detectar quando que uma linha da board está cheia
 -> limpar a linha
 -> fazer o resto cair
 */
@@ -16,18 +16,33 @@ typedef struct pos {
 	int y;
 };
 
-typedef struct bloco {
+typedef struct blocos {
 	pos b[4];
 
-	void criaBlocoI(pos posicao);
-	void criaBlocoJ(pos posicao);
-	void criaBlocoL(pos posicao);
-	void criaBlocoO(pos posicao);
-	void criaBlocoS(pos posicao);
-	void criaBlocoT(pos posicao);
-	void criaBlocoZ(pos posicao);
-}
+	void criaI(pos posicao);
+	void criaJ(pos posicao);
+	void criaL(pos posicao);
+	void criaO(pos posicao);
+	void criaS(pos posicao);
+	void criaT(pos posicao);
+	void criaZ(pos posicao);
+	
+	void giraI();
+	void giraJ();
+	void giraL();
+	void giraO();
+	void giraS();
+	void giraT();
+	void giraZ();
+};
 
 typedef struct board {
-	int[18][10];
+	int casas[18][10];
+	blocos bloco;
+	
+	void limpaBoard();
+	void imprimeBoard();
+	void detectaLinhaCheia();
+	void limpaLinha();
+	void desceLinhas();
 };
